21d6a9f42a229a11bd53c18019fdf4484b5e81b5
Samuca <samuelmadeiradev@gmail.com>
2024-01-18 16:06:21 -0300

    add tailwindcss

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ end
 
 gem "devise", "~> 4.9"
 gem 'bootstrap', '~> 5.2.3'
+gem "tailwindcss-rails", "~> 2.3"
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,45 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  
     def index
-        @tasks = Task.all
+      @tasks = Task.all
     end
-
+  
     def show
     end
-
+  
     def new
-        @task = Task.new
+      @task = Task.new
     end
-
+  
     def create
         @task = Task.new(task_params)
-        if @task.save
-            redirect_to @task, notice: "Tarefs criada com sucesso"
-        else
-            render :new
-        end
+        @task.user_id = current_user
     end
-
     def edit
     end
-
-    def update 
-        if @task.update(task_params)
-            redirect_to @task, notice: "Tarefa atalizada com sucesso"
-        else
-            render :edit
-        end
+  
+    def update
+      if @task.update(task_params)
+        redirect_to @task, notice: 'Tarefa atualizada com sucesso.'
+      else
+        render :edit
+      end
     end
-
+  
     def destroy
-        @task.destroy
-        redirect_to task_url, nitice: "Tarefa excluida com sucesso"
+      @task.destroy
+      redirect_to tasks_url, notice: 'Tarefa excluída com sucesso.'
     end
-
+  
     private
-
+  
     def set_task
-        @task = Task.find(params[:id])
+      @task = Task.find(params[:id])
     end
-
+  
     def task_params
-        params.require(:task).permit(:title, :description)
+      params.require(:task).permit(:title, :description)
     end
-end
+  end
+  
