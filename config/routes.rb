7256fc52caa6cd3a59a67ea84da6d1194a1b72ed
Samuca <samuelmadeiradev@gmail.com>
2024-01-18 04:05:20 -0300

    adjusting routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   end
   devise_for :users
   devise_for :admins
+
+  get 'inicio', to: 'site/welcome#index'
+  root to: 'site/welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
